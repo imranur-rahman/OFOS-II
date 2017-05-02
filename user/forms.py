1298e3a10329c20547f9ff2133ad2281c483f6ab
shimul <ir.shimul@gmail.com>
2017-05-03 00:50:11 +0600

    login and registration form ok, with no extra overhead or "> good to do

--- user/forms.py
+++ user/forms.py
@@ -4,7 +4,13 @@ from .models import Customer
 
 
 class UserLoginForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    #password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(min_length=6, label='', widget=forms.PasswordInput(
+        attrs={'placeholder': 'Password', 'required': 'true', 'class': "form-control"}))
+    username = forms.CharField(label='', min_length=4,
+                               widget=forms.TextInput(
+                                   attrs={'placeholder': 'Username', 'required': 'true', 'class': "form-control",
+                                          'autofocus': 'true'}))
 
     class Meta:
         model = User
@@ -12,8 +18,24 @@ class UserLoginForm(forms.ModelForm):
 
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(min_length=6, label='', widget=forms.PasswordInput(
+        attrs={'placeholder': 'Password', 'required': 'true', 'class': "form-control"}))
+    dummy_password = forms.CharField(min_length=6, label='', widget=forms.PasswordInput(
+        attrs={'placeholder': 'Repeat Password', 'required': 'true', 'class': "form-control"}))
+    username = forms.CharField(label='', min_length=4,
+                               widget=forms.TextInput(
+                                   attrs={'placeholder': 'Username', 'required': 'true', 'class': "form-control",
+                                          'autofocus': 'true'}))
+    email = forms.CharField(label='',
+                            widget=forms.TextInput(
+                                attrs={'placeholder': 'Email', 'required': 'true', 'class': "form-control"}))
+    first_name = forms.CharField(label='',
+                                 widget=forms.TextInput(
+                                     attrs={'placeholder': 'First Name', 'required': 'true', 'class': "form-control"}))
+    last_name = forms.CharField(label='',
+                                widget=forms.TextInput(
+                                    attrs={'placeholder': 'Last Name', 'required': 'true', 'class': "form-control"}))
 
     class Meta:
         model = User
-        fields = ['email', 'username', 'password', 'first_name', 'last_name']
+        fields = ['email', 'username', 'password', 'first_name', 'last_name', 'dummy_password']
